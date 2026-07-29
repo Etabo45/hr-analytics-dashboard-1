776e70e3cce5efd79b73de2d9664e7dcd42897e6
Etabo45 <peninahetabo45@gmail.com>
2026-07-29 18:44:47 +0300

    Create dim_employee table with employee details

---
+++ database/tables/dim_employee.sql
@@ -0,0 +1,11 @@
+CREATE TABLE dim_employee (
+    EmployeeID INT PRIMARY KEY,
+    Age INT,
+    Gender VARCHAR(20),
+    Department VARCHAR(100),
+    JobRole VARCHAR(100),
+    JobLevel INT,
+    YearsAtCompany INT,
+    YearsInCurrentRole INT,
+    YearsSinceLastPromotion INT
+);
